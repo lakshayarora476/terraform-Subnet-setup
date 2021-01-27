61dd80fb46d79a30a545f5c350ac5b83245a2470
Lakshay Arora <53274714+lakshayarora476@users.noreply.github.com>
2021-01-27 16:29:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,21 @@ resource "random_string" "random" {
   special = false
 }
 
+resource "azurerm_resource_group" "TerraformRG" {
+  name     = "subnet-rg"
+  location = "West US"
+}
+
+resource "azurerm_virtual_network" "TerraformVnet" {
+  name                = "subnet_Vnet"
+  location            = azurerm_resource_group.TerraformRG.location
+  resource_group_name = azurerm_resource_group.TerraformRG.name
+  address_space       = var.Vnet_address_prefixes
+}
+
 resource "azurerm_subnet" "TerraformSubnet" {
-  name                 = join("-", [var.subnet_name, random_string.random.result])
-  resource_group_name  = var.subnet_rg_name
-  virtual_network_name = var.subnet_vnet_name
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.TerraformRG.name
+  virtual_network_name = azurerm_virtual_network.TerraformVnet.name
   address_prefixes     = var.subnet_address_prefixes
 }
