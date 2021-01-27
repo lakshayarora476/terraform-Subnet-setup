a559ee0b5c43982e2b33586e3f0655807e05df4c
Lakshay Arora <53274714+lakshayarora476@users.noreply.github.com>
2021-01-27 16:29:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,11 @@
 variable "subnet_name" {}
 
-variable "subnet_rg_name" {}
-
-variable "subnet_vnet_name" {}
-
 variable "subnet_address_prefixes" {
+  type    = list(string)
   default = ["10.0.1.0/24"]
 }
 
 variable "Vnet_address_prefixes" {
+  type    = list(string)
   default = ["10.0.0.0/16"]
 }
